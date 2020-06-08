af55d44ae1de0d54a938056695b19409324aacd6
Daniel <gongsheng@lewanhuyu.com>
2020-06-08 17:13:00 +0800

    project init

---
+++ AliyunPlayerDemo/AliyunPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliyunPlayerDemo
+//
+//  Created by Daniel on 2020/6/8.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AliyunPlayerDemo/AliyunPlayerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AliyunPlayerDemo
+//
+//  Created by Daniel on 2020/6/8.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
